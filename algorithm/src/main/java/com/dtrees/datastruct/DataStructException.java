389dc7e5154dcbae1363ac38833af10aeafdfaac
keithl <liuxkmail@gmail.com>
2017-10-29 17:10:51 +0800

    add study for stack and queue

---
+++ algorithm/src/main/java/com/dtrees/datastruct/DataStructException.java
@@ -0,0 +1,27 @@
+package com.dtrees.datastruct;
+
+/**
+ * Created by keithl on 2017/10/29.
+ */
+
+public class DataStructException extends RuntimeException{
+
+    public DataStructException() {
+    }
+
+    public DataStructException(String message) {
+        super(message);
+    }
+
+    public DataStructException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataStructException(Throwable cause) {
+        super(cause);
+    }
+
+    public DataStructException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
