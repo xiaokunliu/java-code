dd32fba80476e5f054615cdcea74dda4a94fd582
liuxiaokun <liuxiaokun@corp.netease.com>
2018-10-19 20:12:30 +0800

    add java concurrent code

---
+++ base-code/src/main/java/com/books/concurrency_en/sync/arrangeIndepentAttr/Cinema.java
@@ -0,0 +1,9 @@
+package com.books.concurrency_en.sync.arrangeIndepentAttr;
+
+public class Cinema {
+
+    private long vacanciesCinema1;
+    private long vacanciesCinema2;
+
+
+}
--- base-code/src/main/java/com/books/concurrency_en/sync/syncmethod/SyncMethod.java
+++ base-code/src/main/java/com/books/concurrency_en/sync/syncmethod/SyncMethod.java
@@ -20,12 +20,10 @@ public class SyncMethod {
         Bank bank = new Bank(account);
         Thread bankThread = new Thread(bank);
 
-        System.out.printf("Account : Initial Balance: %f\n",account.
-                getBalance());
+        System.out.printf("Account : Initial Balance: %f\n",account.getBalance());
 //        Start the threads
         companyThread.start();
         bankThread.start();
-
         try {
             companyThread.join();
             bankThread.join();
@@ -34,4 +32,21 @@ public class SyncMethod {
             e.printStackTrace();
         }
     }
+
+    /**
+     * We can use the  synchronized keyword to protect the access to a block of code instead
+     of an entire method. We should use the  synchronized keyword in this way to protect the
+     access to the shared data, leaving the rest of operations out of this block, obtaining a better
+     performance of the application. The objective is to have the critical section (the block of code
+     that can be accessed only by one thread at a time) be as short as possible. We have used the
+     synchronized keyword to protect the access to the instruction that updates the number
+     of persons in the building, leaving out the long operations of this block that don't use the
+     shared data. When you use the  synchronized keyword in this way, you must pass an object
+     reference as a parameter. Only one thread can access the  synchronized code (blocks or
+     methods) of that object. Normally, we will use the  this keyword to reference the object that is
+     executing the method
+     *  synchronized (this) {
+         // Java code
+         }
+     */
 }
