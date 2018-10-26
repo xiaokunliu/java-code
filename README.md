b8f9b56c2fbc4dd3b6935447f4fd0b960965cdeb
liuxiaokun <liuxiaokun@corp.netease.com>
2018-10-26 18:29:44 +0800

    add java concurrent code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# java-code
+#### java-code
 for java skills and tips for working
+
+#### 开发 
+
+```
+coding 与 腾讯云绑定
+https://dev.tencent.com/user?welcome
+```
--- base-code/src/main/java/com/dtrees/jdk8/MainClass.java
+++ base-code/src/main/java/com/dtrees/jdk8/MainClass.java
@@ -1,5 +1,7 @@
 package com.dtrees.jdk8;
 
+import java.util.HashMap;
+
 /**
  * Created by keithl on 2017/8/17.
  */
@@ -9,6 +11,5 @@ public class MainClass {
     public static void main(String[] args) {
 
         System.out.println(Float.NEGATIVE_INFINITY);
-
     }
 }
