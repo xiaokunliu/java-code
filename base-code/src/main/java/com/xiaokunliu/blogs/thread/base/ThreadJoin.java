dd033b5f6635ac0dae807388eb9be62c6d216ea3
keithl <kxl602@163.com>
2019-09-25 13:34:19 +0800

    add thread code

---
+++ base-code/src/main/java/com/xiaokunliu/blogs/thread/base/ThreadJoin.java
@@ -0,0 +1,53 @@
+package com.xiaokunliu.blogs.thread.base;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * project:java-code
+ * file:ThreadJoin
+ * package:com.xiaokunliu.blogs.thread.base
+ * date:2019/9/25 12:19
+ * author:keithl
+ */
+public class ThreadJoin {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread dataSourceTask = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(new Date() + " "+ Thread.currentThread().getName() + " start execute data source task ....");
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(new Date() + " "+ Thread.currentThread().getName() + " finish execute data source task ....");
+            }
+        });
+
+
+        Thread networkTask = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(new Date() + " " + Thread.currentThread().getName() + " start execute net work task ....");
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(new Date() +" "+ Thread.currentThread().getName() + " finish execute ney work task ....");
+            }
+        });
+
+        // start two thread
+        dataSourceTask.start();
+        networkTask.start();
+
+
+        dataSourceTask.join();
+        networkTask.join();
+        System.out.println(new Date() + " "+ Thread.currentThread().getName() +" both datasource task and network task have finished");
+    }
+}
